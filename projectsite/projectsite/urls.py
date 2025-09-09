4c0de604c39abac241e05bf15cbca58c77a9f4e7
re_tricks <202280064@psu.palawan.edu.ph>
2025-09-09 23:35:29 +0800

    updated for now

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 """
 from django.urls import path
 from django.contrib import admin
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView,OrganizationUpdateView, OrganizationDeleteView
 from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomePageView.as_view(), name="home"),
-    path("organizations/", OrganizationList.as_view(), name="organization-list"),
+    path("organizations/", OrganizationList.as_view(), name="organization_list"),
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
+    path("organizations/<int:pk>/delete/", OrganizationDeleteView.as_view(), name="organization-delete"),
+
 ]
