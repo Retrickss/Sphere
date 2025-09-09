ed64c43bb5573d522286a8300b4d422b58e22a4c
re_tricks <202280064@psu.palawan.edu.ph>
2025-09-09 23:06:48 +0800

    Listview Checked

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.urls import path
 from django.contrib import admin
-from studentorg.views import HomePageView
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView
+from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomePageView.as_view(), name="home"),
-] 
+    path("organizations/", OrganizationList.as_view(), name="organization-list"),
+    path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
+    path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
+]
